7c17a0ca44c413a81f94f051d61e4632408d6989
Wei Siew <liewws98@gmail.com>
2025-04-02 00:34:17 -0600

    update main and add makefile

--- Src/main.c
+++ Src/main.c
@@ -3,22 +3,14 @@
 
 int main(void)
 {
-  #if defined(LAB1)
-  lab1_main();
-  #elif defined(LAB2)
-  lab2_main();
-  #elif defined(LAB3)
-  lab3_main();
-  #elif defined(LAB4)
-  lab4_main();
-  #elif defined(LAB5)
-  lab5_main();
-  #elif defined(LAB6)
-  lab6_main();
-  #elif defined(LAB7)
-  lab7_main();
-  #else
-  #error No valid target specified
-  #endif
+  HAL_Init();
+  SystemClock_Config();
 
+  HAL_RCC_GPIOC_CLK_ENABLE();
+  
+  while (1) {
+    // Main loop
+  }
+
+  return 1;
 }
