1726d9619dc562b5980d0d0052cdef5618fae5f7
Wei Siew <liewws98@gmail.com>
2025-04-02 16:27:55 -0600

    fix warning

--- Inc/main.h
+++ Inc/main.h
@@ -13,6 +13,7 @@ void HAL_RCC_TIM3_CLK_ENABLE(void);
 void HAL_RCC_USART3_CLK_ENABLE(void);
 void HAL_RCC_I2C2_CLK_ENABLE(void);
 void HAL_RCC_ADC1_CLK_ENABLE(void);
+void HAL_RCC_USART1_CLK_ENABLE(void);
 
 void HAL_config_EXTI(uint32_t line_num, uint32_t control);
 
